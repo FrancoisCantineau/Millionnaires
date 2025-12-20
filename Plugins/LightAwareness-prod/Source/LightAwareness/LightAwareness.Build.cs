edc8f90f09533cacb89be62f5ef6fce404f62ae2
CantineauFrancois <cantineaufrancois@hotmail.com>
2025-12-20 16:22:25 +0100

    add paranoia + invisible enemy

---
+++ Plugins/LightAwareness-prod/Source/LightAwareness/LightAwareness.Build.cs
@@ -0,0 +1,56 @@
+﻿// Light Awareness System. Cem Akkaya https://www.cemakkaya.com
+
+using System.IO;
+using UnrealBuildTool;
+
+public class LightAwareness : ModuleRules
+{
+	public LightAwareness(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				Path.Combine(PluginDirectory,"Content"),
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"RenderCore", 
+				"Renderer",
+				"RHI",
+				"Projects"
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Slate",
+				"SlateCore",
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ Source/Millionnaires/Public/Interfaces/EffectInterface.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EffectInterface.generated.h"
+
+
+UINTERFACE(Blueprintable)
+class UEffectInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class IEffectInterface
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void SanityEffect(
+		float Damage,
+		AActor* DamageCauser
+	);
+};
