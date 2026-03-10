910dbf2c6efadb771498c7f3a7c5a845a64006ae
CantineauFrancois <cantineaufrancois@hotmail.com>
2026-03-10 16:25:37 +0100

    Add room transitions

---
+++ Source/Millionnaires/Private/GameplayAbilitySystem/Data/DA_ProceduralMap.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbilitySystem/Data/DA_ProceduralMap.h"
+
---
+++ Source/Millionnaires/Public/GameplayAbilitySystem/Data/DA_ProceduralMap.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_ProceduralMap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MILLIONNAIRES_API UDA_ProceduralMap : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
